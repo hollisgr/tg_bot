2b5f0fa0df33fe8494de170b67f332a375922c95
hollisgr <hollisgr@student.21-school.ru>
2025-05-06 17:14:37 +0900

    add auth feature

--- internal/bot/handlers/handlers.go
+++ internal/bot/handlers/handlers.go
@@ -17,9 +17,14 @@ var API_URL string
 
 func Register(b *bot.Bot) {
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/get_user ", bot.MatchTypePrefix, GetUserById)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/get_info", bot.MatchTypeExact, GetUserInfo)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/get_userlist", bot.MatchTypeExact, GetUserList)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/create_user ", bot.MatchTypePrefix, CreateUser)
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/delete_user ", bot.MatchTypePrefix, DeleteUserById)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/register_tg_user", bot.MatchTypeExact, RegisterTgUser)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/set_admin ", bot.MatchTypePrefix, SetAdminRole)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/delete_user ", bot.MatchTypePrefix, DeleteUserById)
+
 }
 
 func Help(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -27,10 +32,91 @@ func Help(ctx context.Context, b *bot.Bot, update *models.Update) {
 	func2 := "/get_user {id} to get user by id\n"
 	func3 := "/create_user {username} {password} {email} to create user\n"
 	func4 := "/delete_user {id} to delete user\n"
+	func5 := "/get_info to get user info\n"
+	func6 := "/register_tg_user to register tg_user info\n"
+	func7 := "/set_admin {password} to set admin role  info\n"
 
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
-		Text:   func1 + func2 + func3 + func4,
+		Text:   func1 + func2 + func3 + func4 + func5 + func6 + func7,
+	})
+}
+
+func RegisterTgUser(ctx context.Context, b *bot.Bot, update *models.Update) {
+	username := update.Message.Chat.Username
+	tg_id := update.Message.Chat.ID
+
+	newTgUser := user.TgUser{
+		TG_ID:       tg_id,
+		TG_USERNAME: username,
+	}
+
+	reqBody, _ := json.Marshal(newTgUser)
+
+	RegisterUrl := fmt.Sprintf("%s/tg_users/", API_URL)
+
+	resp, err := http.Post(RegisterUrl, "json", bytes.NewReader(reqBody))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userByte, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   string(userByte),
+	})
+}
+
+func SetAdminRole(ctx context.Context, b *bot.Bot, update *models.Update) {
+
+	body := update.Message.Text
+	password := ""
+	fmt.Sscanf(body, "/set_admin %s", &password)
+
+	newAdmin := user.TgAdmin{
+		TG_ID:     update.Message.Chat.ID,
+		ADMIN_PWD: password,
+	}
+
+	reqBody, _ := json.Marshal(newAdmin)
+
+	AdminUrl := fmt.Sprintf("%s/admin", API_URL)
+
+	resp, err := http.Post(AdminUrl, "json", bytes.NewReader(reqBody))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userByte, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   string(userByte),
+	})
+}
+
+func GetUserInfo(ctx context.Context, b *bot.Bot, update *models.Update) {
+
+	return_string := fmt.Sprintf("tg_id: %d, tg_username: %s, tg_firstname: %s, lastname: %s", update.Message.Chat.ID, update.Message.Chat.Username, update.Message.Chat.FirstName, update.Message.Chat.LastName)
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   return_string,
 	})
 }
 
@@ -95,6 +181,7 @@ func CreateUser(ctx context.Context, b *bot.Bot, update *models.Update) {
 		Username: username,
 		Password: pwd,
 		Email:    email,
+		TG_ID:    update.Message.Chat.ID,
 	}
 
 	reqBody, _ := json.Marshal(newUser)
@@ -125,9 +212,15 @@ func DeleteUserById(ctx context.Context, b *bot.Bot, update *models.Update) {
 	id := 0
 	fmt.Sscanf(body, "/delete_user %d", &id)
 
+	newUser := user.User{
+		TG_ID: update.Message.Chat.ID,
+	}
+
+	reqBody, _ := json.Marshal(newUser)
+
 	delUrl := fmt.Sprintf("%s/users/%d", API_URL, id)
 
-	req, err := http.NewRequest(http.MethodDelete, delUrl, nil)
+	req, err := http.NewRequest(http.MethodDelete, delUrl, bytes.NewReader(reqBody))
 
 	if err != nil {
 		fmt.Println(err)
--- internal/user/user.go
+++ internal/user/user.go
@@ -6,4 +6,15 @@ type User struct {
 	PasswordHash string `json:"passwordHash"`
 	Password     string `json:"password"`
 	Email        string `json:"email"`
+	TG_ID        int64  `json:"telegram_id"`
+}
+
+type TgUser struct {
+	TG_ID       int64  `json:"telegram_id"`
+	TG_USERNAME string `json:"tg_username"`
+}
+
+type TgAdmin struct {
+	TG_ID     int64  `json:"telegram_id"`
+	ADMIN_PWD string `json:"admin_password"`
 }
